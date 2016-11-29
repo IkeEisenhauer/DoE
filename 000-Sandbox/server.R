fe0e32fe12e2daa1fc42f1ab8b91f5c268af5c7c
IkeEisenhauer <wde@pdx.edu>
2016-11-29 12:04:31 -0800

    Update server.R

--- 000-Sandbox/server.R
+++ 000-Sandbox/server.R
@@ -7,26 +7,7 @@ data(iris)
 
 shinyServer(function(input, output) { 
   
-  output$textDisplay <- renderTable({ 
-                                     
-    getMat = matrix(c(paste(input$checkGroup, collapse=','), class(input$checkGroup),
-                      input$boxInput, class(input$boxInput),
-                      as.character(as.Date(input$theDate, origin = "1970-01-01")), class(input$theDate),
-                      paste(as.character(as.Date(input$dateRange[1], origin = "1970-01-01")),
-                            as.character(as.Date(input$dateRange[2], origin = "1970-01-01")), collapse = ','),
-                            class(input$dateRange),
-                      input$pickNumber, class(input$pickNumber),
-                      input$pickRadio, class(input$pickRadio),
-                      input$comboBox, class(input$comboBox),
-                      input$slider, class(input$slider),
-                      input$comment, class(input$comment)
-                      ), ncol=2, byrow = TRUE)
-
-    colnames(getMat) = c("Value", "Class")
-    
-    getMat
-
-     # just a small part of iris for display
+       # just a small part of iris for display
       iris_sample <- iris[sample(nrow(iris), 10),]
       row.names(iris_sample) <- NULL
 
