fa5cdec51155fd7c9ed4df2d90ad4e4241a8397f
IkeEisenhauer <wde@pdx.edu>
2016-11-29 11:39:17 -0800

    Update server.R

--- 000-Sandbox/server.R
+++ 000-Sandbox/server.R
@@ -3,6 +3,7 @@
 ##########################################
 
 library(shiny)
+data(iris)
 
 shinyServer(function(input, output) { 
   
@@ -25,5 +26,26 @@ shinyServer(function(input, output) {
     
     getMat
 
+     # just a small part of iris for display
+      iris_sample <- iris[sample(nrow(iris), 10),]
+      row.names(iris_sample) <- NULL
+
+      # The important part of reactiveValues()
+      values <- reactiveValues()
+      values$df <- iris_sample
+      observe({
+
+        # your action button condition
+        if(input$addButton > 0) {
+          # create the new line to be added from your inputs
+          newLine <- isolate(c(input$sepal.length, input$sepal.width, input$petal.length, input$petal.width, input$species))
+          # update your data
+          # note the unlist of newLine, this prevents a bothersome warning message that the rbind will return regarding rownames because of using isolate.
+          isolate(values$df <- rbind(as.matrix(values$df), unlist(newLine)))
+        }
+      })
+      output$table <- renderTable({values$df}, include.rownames=F)
+    
+    
   })
 })
