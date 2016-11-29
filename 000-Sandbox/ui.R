c76c4de812eb42204981cc089323b7c389470056
IkeEisenhauer <wde@pdx.edu>
2016-11-29 12:03:36 -0800

    Update ui.R

--- 000-Sandbox/ui.R
+++ 000-Sandbox/ui.R
@@ -10,39 +10,6 @@ shinyUI(pageWithSidebar(
   
   sidebarPanel(
     
-    checkboxGroupInput(inputId = "checkGroup",
-                       label = "1. checkboxGroupInput",
-                       choices = list("Ice cream" = "IC", "Trifle" = "Trifle",
-                                      "Pistachios" = "Pist")),
-    
-    checkboxInput(inputId = "boxInput",
-                  label = "2. checkboxInput"),
-    
-    dateInput(inputId = "theDate",
-              label = "3. dateInput"),
-    
-    dateRangeInput(inputId = "dateRange",
-                   label = "4. dateRangeInput"),
-    
-    numericInput(inputId = "pickNumber",
-                 label = "5. numericInput",
-                 min = 1, max = 10, value = 1),
-    
-    radioButtons(inputId = "pickRadio",
-                 label = "6. radioButtons",
-                 choices = list("Taxi" = "Taxi", "Take a walk" = "Walk")),
-    
-    selectInput(inputId = "comboBox",
-                label = "7. selectInput",
-                choices = list("News" = "News", "Situation comedy" = "Sitcom", "Film" = "Film")),
-    
-    sliderInput(inputId = "slider",
-                label = "8. sliderInput",
-                min = 1, max = 10, value = 7, step = 1),
-    
-    textInput(inputId = "comment",
-              label = "9. textInput",
-              value = "")
      textInput("species", label="Add a new species", value="Enter species"),
         numericInput("sepal.length", label="Add a new sepal length", value=""),
         numericInput("sepal.width", label="Add a new speal width", value=""),
@@ -53,7 +20,6 @@ shinyUI(pageWithSidebar(
   
   mainPanel( 
     h3("Output and data type"),
-    tableOutput("textDisplay"),
     tableOutput("table")
   )
 ))
