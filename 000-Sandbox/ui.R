7fe5928da0acd4ad50902886db0bb2580c2f7742
IkeEisenhauer <wde@pdx.edu>
2016-11-30 00:49:27 -0800

    Update ui.R

--- 000-Sandbox/ui.R
+++ 000-Sandbox/ui.R
@@ -6,65 +6,70 @@ library(shiny)
 library(shinydashboard)
 
 dashboardPage(
-  dashboardHeader(title="DoE Simulation 001 - Eisenhauer"),
+  dashboardHeader(
+    title="DoE Simulation 001 - Eisenhauer"
+  ),
   dashboardSidebar(
     sidebarMenu(
       menuItem("Overview", tabName = "Overview", icon = icon("dashboard")),
       menuItem("Process Controls", tabName = "ProcessControls", icon = icon("dashboard")),
       menuItem("Machine Controls", tabName = "MachineControls", icon = icon("dashboard")),
       menuItem("Material Properties", tabName = "MaterialProperties", icon = icon("dashboard")),
-      menuItem("Experimental Results", tabName = "ExperimentalResults", icon = icon("dashboard")),
-      actionButton("addButton", "Run Experiment"),
-      downloadButton("btnDownload", "DOWNLOAD Experimental Data")
+      menuItem("Experimental Results", tabName = "ExperimentalResults", icon = icon("dashboard"))
+      ),
+    actionButton("addButton", "Run Experiment"),
+    downloadButton("btnDownload", "DOWNLOAD Experimental Data")
   ),
-  
   dashboardBody( 
-     tabItems(
-      tabItem(tabName = "Overview",
+    tabItems(
+      tabItem(
+        tabName = "Overview",
         h3("Explaination goes here")
       ),
-      tabItem(tabName = "ProcessControls",
+      tabItem(
+        tabName = "ProcessControls",
         box(
-            title = "Process Controls",
-            status = "primary",
-            solidHeader = TRUE,
-            sliderInput(inputId="A", label="A", min=0, max=3.5,value = 1, step=0.1),
-            sliderInput(inputId="C", label="C", min=0, max=2,value = 1, step=0.1),
-            sliderInput(inputId="D", label="D", min=0, max=5,value = 1, step=0.1),
-            sliderInput(inputId="E", label="E", min=0, max=2.5,value = 1, step=0.1),
-            sliderInput(inputId="I", label="I", min=0.4, max=2,value = 1, step=0.1),
-            sliderInput(inputId="L", label="L", min=0, max=2,value = 1, step=0.1)
+          title = "Process Controls",
+          status = "primary",
+          solidHeader = TRUE,
+          sliderInput(inputId="A", label="A", min=0, max=3.5,value = 1, step=0.1),
+          sliderInput(inputId="C", label="C", min=0, max=2,value = 1, step=0.1),
+          sliderInput(inputId="D", label="D", min=0, max=5,value = 1, step=0.1),
+          sliderInput(inputId="E", label="E", min=0, max=2.5,value = 1, step=0.1),
+          sliderInput(inputId="I", label="I", min=0.4, max=2,value = 1, step=0.1),
+          sliderInput(inputId="L", label="L", min=0, max=2,value = 1, step=0.1)
         )
       ),
       tabItem(tabName = "MachinesControls",
         box(
-            title = "Machine Controls",
-            status = "primary",
-            solidHeader = TRUE,
-            sliderInput(inputId="B", label="B", min=0, max=3,value = 1, step=0.1),
-            sliderInput(inputId="E", label="E", min=0, max=2.5,value = 1, step=0.1),
-            sliderInput(inputId="F", label="F", min=0, max=2,value = 1, step=0.1),
-            sliderInput(inputId="K", label="K", min=0, max=1.2,value = 1, step=0.1),
-            sliderInput(inputId="N", label="N", min=0, max=1.5,value = 1, step=0.1),
-            sliderInput(inputId="Q", label="Q", min=0, max=10,value = 1, step=0.1)
+          title = "Machine Controls",
+          status = "primary",
+          solidHeader = TRUE,
+          sliderInput(inputId="B", label="B", min=0, max=3,value = 1, step=0.1),
+          sliderInput(inputId="E", label="E", min=0, max=2.5,value = 1, step=0.1),
+          sliderInput(inputId="F", label="F", min=0, max=2,value = 1, step=0.1),
+          sliderInput(inputId="K", label="K", min=0, max=1.2,value = 1, step=0.1),
+          sliderInput(inputId="N", label="N", min=0, max=1.5,value = 1, step=0.1),
+          sliderInput(inputId="Q", label="Q", min=0, max=10,value = 1, step=0.1)
         )
       ),
       tabItem(tabName = "MaterialProperties",
         box(
-            title = "Material Properties",
-            status = "primary",
-            solidHeader = TRUE,
-            sliderInput(inputId="G", label="G", min=0, max=2.5,value = 1, step=0.1),
-            sliderInput(inputId="H", label="H", min=0, max=2,value = 1, step=0.1),
-            sliderInput(inputId="J", label="J", min=0, max=3,value = 1, step=0.1),
-            sliderInput(inputId="M", label="M", min=0, max=2,value = 1, step=0.1),
-            sliderInput(inputId="O", label="O", min=0.4, max=3.5,value = 1, step=0.1),
-            sliderInput(inputId="P", label="P", min=0, max=2.0,value = 1, step=0.1)
+          title = "Material Properties",
+          status = "primary",
+          solidHeader = TRUE,
+          sliderInput(inputId="G", label="G", min=0, max=2.5,value = 1, step=0.1),
+          sliderInput(inputId="H", label="H", min=0, max=2,value = 1, step=0.1),
+          sliderInput(inputId="J", label="J", min=0, max=3,value = 1, step=0.1),
+          sliderInput(inputId="M", label="M", min=0, max=2,value = 1, step=0.1),
+          sliderInput(inputId="O", label="O", min=0.4, max=3.5,value = 1, step=0.1),
+          sliderInput(inputId="P", label="P", min=0, max=2.0,value = 1, step=0.1)
         )
       ),
       tabItem(tabName = "ExperimentalResults",
         h3("Experimental Results"),
         tableOutput("table")
       )
-     )
+    )
   )
+)
