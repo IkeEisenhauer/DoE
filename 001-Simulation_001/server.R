58094d829d43871f3586a571dc687164c11c28cf
IkeEisenhauer <wde@pdx.edu>
2017-02-01 23:09:04 -0800

    Update server.R

--- 001-Simulation_001/server.R
+++ 001-Simulation_001/server.R
@@ -8,8 +8,8 @@ shinyServer(function(input, output) {
 
   # Reset Data
     exp_data = read.table(text="",
-                         col.names=c("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","YLD"),
-                          colClasses = c("double","double","double","double",
+                         col.names=c("IDX","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","YLD"),
+                          colClasses = c("double","double","double","double","double",
                                         "double","double","double","double",
                                         "double","double","double","double",
                                         "double","double","double","double",
@@ -26,7 +26,7 @@ shinyServer(function(input, output) {
           isolate(YLD <- sqrt(abs(15*(2-(input$A-1)^2-(input$F-1)^2))) + 15*(input$B)^2*exp(0.64-(input$B)^2-10*(input$B-input$G)^2) + 50*input$C*exp(0.04-(input$C)^2-10*(6*input$C-input$H)^2) + 0.5*input$D*input$E + 5*abs(input$I-input$K) + (input$J)^2 + (input$M)^2 - (input$J*input$M) + 10*exp(-100*((0.72*input$L-1.3)^2+(input$N-1.3)^2)) + rnorm(1,0,1))
           
           # create the new line to be added from your inputs
-          newLine <- isolate(c(input$A,
+          newLine <- isolate(c(nrow(as.matrix(values$df))+1,input$A,
                                input$B,
                                input$C,
                                input$D,
