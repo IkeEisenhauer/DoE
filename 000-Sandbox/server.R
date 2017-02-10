1bd4fcca09ed241ea78a778769aa530581e64b40
IkeEisenhauer <wde@pdx.edu>
2017-02-10 08:28:47 -0800

    Moving to new example

--- 000-Sandbox/server.R
+++ 000-Sandbox/server.R
@@ -1,63 +1,284 @@
-##########################################
-##### DoE Game 1 [Based on Diamond] - server.R ###
-##########################################
+# server.R
+
+# routine to provide the info that will be displayed
+
+# in each tab defined in ui.R
+
+
+# include require libraries
 
 library(shiny)
+library(nortest)
+library(agricolae)
+library(car)
 
-shinyServer(function(input, output) { 
 
-  # Reset Data
-    exp_data = read.table(text="",
-                         col.names=c("INDEX", "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","YLD"),
-                          colClasses = c("double","double","double","double","double",
-                                        "double","double","double","double",
-                                        "double","double","double","double",
-                                        "double","double","double","double",
-                                        "double","double","double") 
-                         )
+#########################################
+
+# Main function for shinyServer:        #
+# ANOVA utils
+# defines all functions to populate     #
+
+# objects set up in ui.R                #
+
+#########################################
+
+shinyServer(function(input, output) {
   
-  # The important part of reactiveValues()
-    values <- reactiveValues()
-    values$df <- exp_data
-    observe({
-        # your action button condition
-        if(input$addButton > 0) {
-          # Create Yield
-          isolate(YLD <- sqrt(abs(15*(2-(input$A-1)^2-(input$F-1)^2))) + 15*(input$B)^2*exp(0.64-(input$B)^2-10*(input$B-input$G)^2) + 50*input$C*exp(0.04-(input$C)^2-10*(6*input$C-input$H)^2) + 0.5*input$D*input$E + 5*abs(input$I-input$K) + (input$J)^2 + (input$M)^2 - (input$J*input$M) + 10*exp(-100*((0.72*input$L-1.3)^2+(input$N-1.3)^2)) + 0*rnorm(1,0,1))
-          
-          # create the new line to be added from your inputs
-          newLine <- isolate(c(input$A,
-                               input$B,
-                               input$C,
-                               input$D,
-                               input$E,
-                               input$F,
-                               input$G,
-                               input$H,
-                               input$I,
-                               input$J,
-                               input$K,
-                               input$L,
-                               input$M,
-                               input$N,
-                               input$O,
-                               input$P,
-                               input$Q,
-                               input$R,
-                               YLD))
-          # update your data
-          # note the unlist of newLine, this prevents a bothersome warning message that the rbind will return regarding rownames because of using isolate.
-          isolate(values$df <- rbind(as.matrix(values$df), unlist(newLine)))
-        }
-      })
-
-      output$btnDownload <- downloadHandler(
-        filename = function() {paste("Experimental_Data_001", ".csv", sep='')},
-        content = function(file) {
-          write.csv(values$df,file)
-        }
-      )  
-                            
-      output$table <- renderTable({values$df}, include.rownames=F)
-        
+  
+  
+  
+  
+  
+  
+  ################################################################# 
+  
+  # Compute the forumla text in a reactive expression since it is # 
+  
+  # shared by the output$caption and output$mpgPlot expressions   #
+  
+  #################################################################
+  
+  formulaText <- reactive({
+    
+    paste("Data set:", input$vectorname)
+    
   })
+  
+  ##  CI.H <- meanin+1.96*stdin # upper CI 
+  #  CI.L <- meanin-1.96*stdin     # Calculate lower CI 
+  #  xvals <- barplot(meanin, ylim=ylim, main="Interval Plot",...) # Plot bars 
+  #  arrows(xvals, meanin, xvals, CI.H, angle=90) # Draw error bars 
+  #  arrows(xvals, meanin, xvals, CI.L, angle=90)
+  #  
+ # }
+#  
+  
+  
+  ##############################################################
+  # returns dataset and variable name based on text entered in #
+  # input$vectorname and input$varname                         #
+  ##############################################################
+  
+  whichdataset <- reactive({
+    
+    dataset <- get(input$vectorname) 
+    #if (input$varname!="") dataset <- with(get(input$vectorname),get(input$varname))
+    return(dataset)
+    
+  })
+  
+  
+
+############################################################
+# return the name for the response variable from the model #
+# string                                                   #
+############################################################
+
+  get_resp_var <- reactive({
+     return(as.character(formula(input$modstr))[2])  
+  })
+  
+  get_factors <- reactive ({
+    
+    factorstr <- as.character(formula(input$modstr))[3]
+    return(sub("\\s","",unlist(strsplit(factorstr,"[*+:]"))))
+    
+  })
+
+  
+  ######################################################
+  # Return the formula text for printing as a caption  #
+  ######################################################
+  
+  output$caption <- renderText({
+    
+    formulaText()
+    
+  })
+  
+  
+    
+  
+  
+  #######################################################
+  # show data in the select vector/dataset and variable #
+  #######################################################
+  
+  output$view <- renderPrint({
+    
+    whichdataset()
+    
+  })
+  #######################################################
+  # Display the structure of the selected data frame    #
+  #######################################################
+  output$structure <- renderPrint({
+    
+    dataset=whichdataset()
+    str(dataset)
+  }) 
+  
+  #######################################################
+  # Display the normality of the selected data frame    #
+  #######################################################
+  output$normality <- renderPrint({
+    
+    dataset <- whichdataset()
+    resp_var <- get_resp_var()
+    factors <- get_factors()
+    
+    
+      if (length(factors)==1) { 
+          print(with(dataset,tapply(get(resp_var), get(factors[1]), shapiro.test)))
+        }
+    
+    if (length(factors)==2) {
+        f1=sub("^\\s+|\\s+$", "", factors[1])
+        f2=sub("^\\s+|\\s+$", "", factors[2])
+        print(with(dataset,tapply(get(resp_var), interaction(get(f1),get(f2)), shapiro.test)) )
+                                                                        
+    }
+ 
+    
+  })  
+  
+  #######################################################
+  # Display the ANOVA Table                             #
+  #######################################################
+  
+  output$anovatable <- renderPrint({
+    
+    dataset=whichdataset()
+    aov.model<-aov(formula(input$modstr), data=dataset)
+    print(aov.model)
+    br()
+    br()
+    print(summary(aov.model))
+    cat("Coefficients"); cat("\n")
+    print(aov.model$coefficients)
+    
+  }) 
+  
+  
+  
+  
+  
+  
+  ################################################
+  # Generate a boxplot of the requested variable #
+  # include outliers                             #
+  ################################################
+  output$gsPlot <- renderPlot({
+    
+    dataset=whichdataset()
+    mt=paste(input$vectorname, " : " ,input$modstr)
+    f=formula(input$modstr)
+    boxplot(f,data=dataset, main=mt, col="green",
+            ylab=as.character(f)[2], xlab=as.character(f)[3])
+    
+  }) 
+  
+#   output$iaplot <- renderPlot({
+#   dataset=whichdataset()
+#   interaction.plot(tension, wool, breaks, typ="l"))
+#   
+#     
+#   })
+#   
+  
+  #######################################################
+  # plot the residuals                                  #
+  #######################################################
+  output$resid_plot <- renderPlot({
+    
+    dataset<-whichdataset()
+    aov.model<-aov(formula(input$modstr), data=dataset)
+    pv<-(ad.test(aov.model$residuals)$p.value)
+    layout(matrix(c(1,2,3,4),2,2, byrow = TRUE))
+    
+    hist(aov.model$residuals, col="green", main=paste("Anderson Darling P-value= ", round(pv,4)))
+    plot(aov.model$fitted, aov.model$residuals);  abline(h=0, col="red")
+    plot(aov.model, which=2)
+    plot(aov.model$residuals)
+  })
+  
+  #######################################################
+  # plot Tukey Groups                                   #
+  #######################################################
+  output$tukeyplot <- renderPlot({
+    
+    dataset<-whichdataset()
+    aov.model<-aov(formula(input$modstr), data=dataset)
+    #out<-HSD.test(aov.model, unlist(strsplit(input$tukeystr," ")))
+    out<-HSD.test(aov.model, get_factors())
+    par(cex=1, mar=c(3,8,1,1))
+    
+    #bar.group(out$groups)      
+    bar.group(out$groups,horiz=T,col="blue",
+              xlim=c(0,max(out$means[,1]*1.2)),las=1)
+    
+  })
+  
+  
+  
+  output$tukey <- renderPrint({
+    
+    dataset=whichdataset()
+    aov.model<-aov(formula(input$modstr), data=dataset)
+    #out<-HSD.test(aov.model, unlist(strsplit(input$tukeystr," ")))
+   out <-HSD.test(aov.model, get_factors(), " ")
+    print(out)
+    
+  })  
+  
+  output$fvalue <- renderPrint({
+    cat(paste("F(.05,", input$dfnum, ",", input$dfdem,")=",qf(.95,input$dfnum,input$dfdem)))  
+    
+  }) 
+  
+  output$equalvar <- renderPrint({
+    dataset=whichdataset()
+    f=formula(input$modstr)
+    
+    if (length(strsplit(as.character(f)[3],split="*", fixed=T)[[1]])==2) {
+      astr=strsplit(as.character(f)[[3]][1],"*",fixed=T)[[1]][1]
+      bstr=strsplit(as.character(f)[[3]][1],"*",fixed=T)[[1]][2]
+      a=with(dataset,get(strsplit(astr,split=" ")[[1]][1]))
+      b=with(dataset,get(strsplit(bstr, split=" ")[[1]][2]))
+      myf = formula(paste(as.character(f)[2],"~ interaction(a,b)"))
+      
+      print(bartlett.test(myf, data=dataset))  
+    }
+   
+    if (length(strsplit(as.character(f)[3],split="*", fixed=T)[[1]])==1) {
+        print(bartlett.test(f, data=dataset))
+    }
+   
+    br()
+    cat(paste("--------------------------------------------------","\n\n"))
+
+    print(leveneTest(f, data=dataset))
+  }) 
+  
+  output$help <- renderPrint({
+    
+    cat("One way Anove with response Y and Factor A \n model Y~A"); cat("\n\n")
+    cat("Two way Anova with response Y and Factors A,B \n model Y~A+B "); cat("\n\n")
+    cat("Two way Anova with response Y and Factors A,B and interaction (crossed)\n")
+    cat(" model : Y~A*B   or \n model : Y~A+B+A:B"); cat("\n\n")
+    cat("Two way ANOVA with response Y and Factors B nested in A\n")
+    cat(" model : Y~A/B   or \n model : Y~A+A:B"); cat("\n\n")
+    cat("One way with response Y and Random factor A\n")
+    cat(" model : Y~Error(A)"); cat("\n\n")
+    cat("Two Way with response Y and Random factor A and Fixed factor B\n")
+    cat(" model : Y~Error(A) + B")
+    
+    
+    
+  })
+  
+  
+}) # end of shinyServer
+
+
--- 000-Sandbox/ui.R
+++ 000-Sandbox/ui.R
@@ -1,81 +1,49 @@
-##############################
-### DoE Game 1 based on Diamond - ui.R ########
-##############################
+# ui.R
 
-library(shiny) 
-library(shinydashboard)
+library(shiny)
+library(nortest)
+library(agricolae)
+library(car)
 
-dashboardPage(
-  skin="green",
-  dashboardHeader(
-    title="DoE Simulation 001 - Eisenhauer",
-    titleWidth = 350
+shinyUI(fluidPage(
+  
+    
+  # Application title
+  
+  headerPanel("ANOVA Tools"),
+    
+  sidebarPanel( textInput("vectorname", "Enter Data frame: ", "warpbreaks"),
+                textInput("modstr",     "Enter Model: ", "breaks~tension"),
+                #textInput("tukeystr", "Enter list of factors to Tukey test",""),
+                #numericInput("dfnum", "df Numerator",""),
+                #numericInput("dfdem", "df Demoninator", ""),
+                
+                submitButton("Update View")          
+                
   ),
-  dashboardSidebar(
-    sidebarMenu(width=350,
-      menuItem("Overview", tabName = "Overview", icon = icon("info-circle")),
-      menuItem("Process Controls", tabName = "ProcessControls", icon = icon("industry")),
-      menuItem("Machine Controls", tabName = "MachineControls", icon = icon("steam")),
-      menuItem("Material Properties", tabName = "MaterialProperties", icon = icon("flask")),
-      menuItem("Experimental Results", tabName = "ExperimentalResults", icon = icon("table"))
-      ),
-    HTML("<hr>"),
-    actionButton("addButton", "Run Experiment", icon=icon("play"))
-  ),
-  dashboardBody( 
-    tabItems(
-      tabItem(
-        tabName = "Overview",
-        h3("Explaination goes here")
-      ),
-      tabItem(
-        tabName = "ProcessControls",
-        div(style="height: 1200px;",box(
-          width = 750,
-          title = "Process Controls",
-          status = "primary",
-          solidHeader = TRUE,
-          div(style="height: 150px;",sliderInput(inputId="A", label="A", min=0, max=3.5,value = 1, step=0.1)),
-          div(style="height: 150px;",sliderInput(inputId="C", label="C", min=0, max=2,value = 1, step=0.1)),
-          div(style="height: 150px;",sliderInput(inputId="D", label="D", min=0, max=5,value = 1, step=0.1)),
-          div(style="height: 150px;",sliderInput(inputId="I", label="I", min=0.4, max=2,value = 1, step=0.1)),
-          div(style="height: 150px;",sliderInput(inputId="L", label="L", min=0, max=2,value = 1, step=0.1)),
-          div(style="height: 150px;",sliderInput(inputId="R", label="R", min=0, max=18,value = 1, step=0.1))
-        ))
-      ),
-      tabItem(tabName = "MachineControls",
-        div(style="height: 1200px;",box(
-          width = 750,
-          title = "Machine Controls",
-          status = "primary",
-          solidHeader = TRUE,
-          div(style="height: 150px;",sliderInput(inputId="B", label="B", min=0, max=3,value = 1, step=0.1)),
-          div(style="height: 150px;",sliderInput(inputId="E", label="E", min=0, max=2.5,value = 1, step=0.1)),
-          div(style="height: 150px;",sliderInput(inputId="F", label="F", min=0, max=2,value = 1, step=0.1)),
-          div(style="height: 150px;",sliderInput(inputId="K", label="K", min=0, max=1.2,value = 1, step=0.1)),
-          div(style="height: 150px;",sliderInput(inputId="N", label="N", min=0, max=1.5,value = 1, step=0.1)),
-          div(style="height: 150px;",sliderInput(inputId="Q", label="Q", min=0, max=10,value = 1, step=0.1))
-        ))
-      ),
-      tabItem(tabName = "MaterialProperties",
-        div(style="height: 1200px;",box(
-          width = 750,
-          title = "Material Properties",
-          status = "primary",
-          solidHeader = TRUE,
-          div(style="height: 150px;",sliderInput(inputId="G", label="G", min=0, max=2.5,value = 1, step=0.1)),
-          div(style="height: 150px;",sliderInput(inputId="H", label="H", min=0, max=2,value = 1, step=0.1)),
-          div(style="height: 150px;",sliderInput(inputId="J", label="J", min=0, max=3,value = 1, step=0.1)),
-          div(style="height: 150px;",sliderInput(inputId="M", label="M", min=0, max=2,value = 1, step=0.1)),
-          div(style="height: 150px;",sliderInput(inputId="O", label="O", min=0.4, max=3.5,value = 1, step=0.1)),
-          div(style="height: 150px;",sliderInput(inputId="P", label="P", min=0, max=2.0,value = 1, step=0.1))
-        ))
-      ),
-      tabItem(tabName = "ExperimentalResults",
-        h3("Experimental Results"),
-        downloadButton("btnDownload", "Download Experimental Data"),
-        tableOutput("table")
-      )
+    
+  mainPanel(
+    
+  
+    h2(textOutput("caption")),
+    
+   tabsetPanel(
+      tabPanel("Show Data",         verbatimTextOutput("view")),
+      tabPanel("Structure",         verbatimTextOutput("structure")),
+      tabPanel("Normality",         verbatimTextOutput("normality")),
+      tabPanel("Box Plot",          plotOutput("gsPlot")),
+      tabPanel("Interaction Plot",  plotOutput("iaplot")),
+      tabPanel("ANOVA Table",       verbatimTextOutput("anovatable")),
+      tabPanel("Tukey Comparisons", plotOutput("tukeyplot"), verbatimTextOutput("tukey")),
+      #tabPanel("F value",           verbatimTextOutput("fvalue")),
+      tabPanel("Equal Variance",    verbatimTextOutput("equalvar")),
+      tabPanel("Residual",          plotOutput("resid_plot")),
+      tabPanel("Help",              verbatimTextOutput("help"))
     )
+        
   )
-)
+  
+  
+  
+  
+))
