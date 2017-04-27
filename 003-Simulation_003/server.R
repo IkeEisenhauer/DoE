06865ac9b3c1f998db408dd2956326ceb419925c
IkeEisenhauer <wde@pdx.edu>
2017-04-27 09:29:34 -0700

    New sim
    Newone to add, simplified

---
+++ 003-Simulation_003/server.R
@@ -0,0 +1,47 @@
+##########################################
+##### DoE Game 3 [Based on Diamond] - server.R ###
+##########################################
+
+library(shiny)
+
+shinyServer(function(input, output) { 
+
+  # Reset Data
+    exp_data = read.table(text="",
+                         col.names=c("IDX","C","F","G","K","YLD"),
+                          colClasses = c("double","double","double","double","double",
+                                        "double") 
+                         )
+  
+  # The important part of reactiveValues()
+    values <- reactiveValues()
+    values$df <- exp_data
+    observe({
+        # your action button condition
+        if(input$addButton > 0) {
+          # Create Yield
+          isolate(YLD <- 9-3(abs(input$F+input$G-3))+4*(1+(input$C-1.5)^2-(input$K-1)^2-0.2*(input$C-1.5)*(input$K-1)) + rnorm(1,0,2))
+          
+          # create the new line to be added from your inputs
+          newLine <- isolate(c(nrow(as.matrix(values$df))+1,
+                               input$C,
+                               input$F,
+                               input$G,
+                               input$K,
+                               YLD))
+          # update your data
+          # note the unlist of newLine, this prevents a bothersome warning message that the rbind will return regarding rownames because of using isolate.
+          isolate(values$df <- rbind(as.matrix(values$df), unlist(newLine)))
+        }
+      })
+
+      output$btnDownload <- downloadHandler(
+        filename = function() {paste("Experimental_Data_003", ".csv", sep='')},
+        content = function(file) {
+          write.csv(values$df,file)
+        }
+      )  
+                            
+      output$table <- renderTable({values$df}, include.rownames=F)
+        
+  })
---
+++ 003-Simulation_003/ui.R
@@ -0,0 +1,50 @@
+##############################
+### DoE Game 3 based on Diamond - ui.R ########
+##############################
+
+library(shiny) 
+library(shinydashboard)
+
+dashboardPage(
+  skin="green",
+  dashboardHeader(
+    title="DoE Simulation 003 - Eisenhauer",
+    titleWidth = 350
+  ),
+  dashboardSidebar(
+    width = 350,
+    sidebarMenu(
+      menuItem("Overview", tabName = "Overview", icon = icon("info-circle")),
+      menuItem("Control", tabName = "Inputs", icon = icon("industry")),
+      menuItem("Experimental Results", tabName = "ExperimentalResults", icon = icon("table"))
+      ),
+    HTML("<hr>"),
+    actionButton("addButton", "Run Experiment", icon=icon("play"))
+  ),
+  dashboardBody( 
+    tabItems(
+      tabItem(
+        tabName = "Overview",
+        h3("Simulation for Hands On Exeercises STAT466")
+      ),
+      tabItem(
+        tabName = "Inputs",
+        div(style="height: 1200px;",box(
+          width = 750,
+          title = "Controls",
+          status = "primary",
+          solidHeader = TRUE,
+          div(style="height: 150px;",TextInput(inputId="C", label="C")),
+          div(style="height: 150px;",TextInput(inputId="F", label="F")),
+          div(style="height: 150px;",TextInput(inputId="G", label="G")),
+          div(style="height: 150px;",TextInput(inputId="K", label="K"))
+        ))
+      ),
+      tabItem(tabName = "ExperimentalResults",
+        h3("Experimental Results"),
+        downloadButton("btnDownload", "Download Experimental Data"),
+        tableOutput("table")
+      )
+    )
+  )
+)
